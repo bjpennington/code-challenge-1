449b6256c5db1d064a7481ee5c8447999f99c71e
BJ Pennington <bjpennington112@gmail.com>
2018-06-25 13:18:36 -0500

    set up generate button

--- scripts/client.js
+++ scripts/client.js
@@ -4,4 +4,13 @@ $(readyNow);
 
 function readyNow() {
     console.log('jq');
+    addEventListeners();
+}
+
+function addEventListeners() {
+    $('#genButton').on('click', handleGenerate);
+}
+
+function handleGenerate () {
+    console.log('You clicked it!');
 }
